de77dd3f2c579a6b930a017bb147774242476d56
Your Name <rjtnamdev565@>
2020-07-19 20:55:53 +0530

    dd

---
+++ src/app/service/authentication.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
+import { Observable, of, BehaviorSubject } from 'rxjs'
+import { map } from 'rxjs/operators'
+import { Router } from '@angular/router'
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { User } from '../model/user';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+
+  private currentUserSubject: BehaviorSubject<User>;
+  public currentUser: Observable<User>;
+
+  constructor(private http: HttpClient) {
+      this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
+      this.currentUser = this.currentUserSubject.asObservable();
+  }
+
+  public get currentUserValue(): User {
+      return this.currentUserSubject.value;
+  }
+  public register(user: any): Observable<any> {
+    //console.log(user);
+    return this.http.post(`https://www.pokersportsleague.com/api/register`, user, {
+      headers: {'Content-Type': 'application/json'}
+    })
+}
+  login(username: string, password: string) {
+      return this.http.post<any>(` localhost:3000/register`, { username, password })
+          .pipe(map(user => {
+              // login successful if there's a jwt token in the response
+              if (user && user.token) {
+                  // store user details and jwt token in local storage to keep user logged in between page refreshes
+                  localStorage.setItem('currentUser', JSON.stringify(user));
+                  this.currentUserSubject.next(user);
+              }
+
+              return user;
+          }));
+  }
+
+
+  logout() {
+      // remove user from local storage to log user out
+      localStorage.removeItem('currentUser');
+      this.currentUserSubject.next(null);
+  }
+}
