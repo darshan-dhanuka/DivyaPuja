e102a1515765a90071da76788b2f149184d5347d
Your Name <rjtnamdev565@>
2020-07-19 20:56:25 +0530

    dd

---
+++ src/app/service/user.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { User } from '../model/user';
+
+@Injectable({ providedIn: 'root' })
+export class UserService {
+    constructor(private http: HttpClient) { }
+
+    getAll() {
+        return this.http.get<User[]>(`/users`);
+    }
+
+    register(user: User) {
+        return this.http.post(`localhost:3000/register`, user);
+    }
+
+    delete(id: number) {
+        return this.http.delete(`localhost:3000/users/${id}`);
+    }
+}
