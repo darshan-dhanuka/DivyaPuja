6bb84424624d6f9f10b7486b5adc2120a92b0f83
Your Name <rjtnamdev565@>
2020-07-24 22:50:49 +0530

    ss

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,10 +7,11 @@ import { RegisterComponent } from './register/register.component';
 
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-
+  // canActivate: [AuthGuard]
   // otherwise redirect to home
   { path: '**', redirectTo: '' }
 ];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,11 +5,15 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  togglefp() {
+    document.getElementById('divshow2').style.display = 'none';
+    document.getElementById('divfp').style.display = 'block';
+  }
 }
