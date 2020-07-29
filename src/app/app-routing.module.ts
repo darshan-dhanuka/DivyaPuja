cca9cd4ddb4f0122ae5ed49f9379bb8f184adbd7
Your Name <rjtnamdev565@>
2020-07-30 00:20:06 +0530

    dd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './home/home.component';
 import { AuthGuard } from './service/auth-guard.service';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { PanditFormComponent } from './pandit-form/pandit-form.component';
 
 
 const routes: Routes = [
@@ -11,6 +12,7 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'pandit-form', component: PanditFormComponent },
   // canActivate: [AuthGuard]
   // otherwise redirect to home
   { path: '**', redirectTo: '' }
--- src/app/pandit-form/pandit-form.component.ts
+++ src/app/pandit-form/pandit-form.component.ts
@@ -1,15 +1,122 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
 
+import { DataService } from '../service/data.service';
+import { User } from '../model/user';
+import {AlertService} from '../service/alert.service';
+import { UserService } from '../service/user.service';
+import { ImageServiceService } from '../service/image-service.service';
+
+class ImageSnippet {
+  pending: boolean = false;
+  status: string = 'init';
+
+  constructor(public src: string, public file: File) {}
+}
 @Component({
   selector: 'app-pandit-form',
   templateUrl: './pandit-form.component.html',
   styleUrls: ['./pandit-form.component.css']
 })
 export class PanditFormComponent implements OnInit {
+  panditForm: FormGroup;
+  loading = false;
+  submitted = false;
+  selectedFile: ImageSnippet;
+ 
+  constructor(
+    private formBuilder: FormBuilder,
+      private router: Router,
+      private dataService: DataService,
+      private alertService: AlertService,
+      private imageService: ImageServiceService,
+  ) { }
+// pandit_name, email_id, mobile_num ,address ,city,dob ,languages_known ,experience, , pathshala_name, pandit_cat, about ,photo_url
+  ngOnInit() {
+    this.panditForm = this.formBuilder.group({
+      pandit_name: ['', Validators.required],
+      email_id: ['', Validators.required],
+      mobile_num: ['', Validators.required],
+      address: ['', [Validators.required]],
+      city: ['', Validators.required],
+      dob: ['', Validators.required],
+      languages_known: ['', Validators.required],
+      experience: ['', Validators.required],
+      adhar_num: ['', Validators.required],
+      pathshala_name: ['', Validators.required],
+      pandit_cat: ['', Validators.required],
+      about: ['', Validators.required],
+      photo_url: ['', Validators.required],
+      
+    });
+  
+}
+private onSuccess() {
+  this.selectedFile.pending = false;
+  this.selectedFile.status = 'ok';
+}
 
-  constructor() { }
+private onError() {
+  this.selectedFile.pending = false;
+  this.selectedFile.status = 'fail';
+  this.selectedFile.src = '';
+}
+processFile(imageInput: any) {
+  const file: File = imageInput.files[0];
+  const reader = new FileReader();
 
-  ngOnInit(): void {
+  reader.addEventListener('load', (event: any) => {
+
+    this.selectedFile = new ImageSnippet(event.target.result, file);
+
+    this.imageService.uploadImage(this.selectedFile.file).subscribe(
+      (res) => {
+        console.log(res)
+      //  this.photo_url = res.id
+      
+      },
+      (err) => {
+      
+      })
+  });
+
+  reader.readAsDataURL(file);
+}
+
+
+ // convenience getter for easy access to form fields
+ get f() { return this.panditForm.controls; }
+
+
+ Submit() {
+  this.submitted = true;
+
+  // stop here if form is invalid
+  if (this.panditForm.invalid) {
+      return;
   }
 
+  this.loading = true;
+  
+  this.dataService.panditRegister(this.panditForm.value)
+      .pipe(first())
+      .subscribe(
+          data => {
+              this.alertService.success('Registration successful', true);
+              window.location.reload();
+              this.router.navigate(["/home"]);
+              // document.getElementById('divshow2').style.display = 'none';
+
+          },
+          error => {
+              //this.alertService.error(error);
+              this.loading = false;
+          });
 }
+
+}
+
+
+
