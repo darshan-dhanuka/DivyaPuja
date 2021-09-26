df450d8088cae8d9050422d3e4dfc7fa2de1f983
vijaybodake12 <vijaybodake12@gmail.com>
2021-09-26 15:57:00 +0530

    Story Description: Added current date on zodiac page.

--- src/app/zodiac/zodiac.component.ts
+++ src/app/zodiac/zodiac.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./zodiac.component.css']
 })
 export class ZodiacComponent implements OnInit {
-
+  todaysDate=new Date();
   constructor() { }
 
   ngOnInit(): void {
